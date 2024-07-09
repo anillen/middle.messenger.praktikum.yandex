9a290893bfe3f5c350de5dea3953b23a0c3ea4a7
anillen <daniil_telenkov@vk.com>
2024-07-09 15:01:23 +0300

    initial

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import handlebars from "vite-plugin-handlebars";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+
+    build: {
+        rollupOptions: {
+            input: {
+                chats: resolve(__dirname, "src/pages/chats/index.html"),
+                login: resolve(__dirname, "src/pages/login/index.html"),
+                registration: resolve(__dirname, "src/pages/registration/index.html"),
+                account: resolve(__dirname, "src/pages/account/index.html"),
+            }
+        },
+    },
+
+    server: {
+        port: 3004
+    },
+
+    plugins: [handlebars({
+        context: {
+            title: "test"
+        },
+        partialDirectory: resolve(__dirname, "src/partials")
+    })]
+    
+})
