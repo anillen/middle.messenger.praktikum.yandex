fa367eccd143cb58384ec465c9ea293207a27ba9
anillen <daniil_telenkov@vk.com>
2024-07-11 12:27:20 +0300

    add empty row in files

--- src/pages/account/components/switch-password-container/switch-password-container.ts
+++ src/pages/account/components/switch-password-container/switch-password-container.ts
@@ -2,3 +2,4 @@ export const switchPasswordContainer=
 `{{> input type="password" labelText="Старый пароль" name="oldPassword"}} 
 {{> input type="password" labelText="Новый пароль"}}
 {{> input type="password" labelText="Подтверждение пароля" name="newPassword"}}`;
+ 
